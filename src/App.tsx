c006359eba93c4398af9a6138875e3f6f6773043
Kaique Campos <kaiquecamposferreira@gmail.com>
2022-04-13 12:17:47 -0300

    add title to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { CalculateCallValueButton } from './components/CalculateCallValueButton'
 import { CallInformation } from './components/CallInformations'
 import { FinalValues } from './components/FinalValues'
+import { Title } from './components/Title'
 import styles from "./styles/app.module.scss"
 import "./styles/global.scss"
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className={styles.container}>
 
+      <Title/>
       <CallInformation />
       <FinalValues />
       <CalculateCallValueButton />
---
+++ src/components/Title/index.tsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+export function Title() {
+    return(
+        <h1
+            className={styles.title}
+        >LOLDESIGN - Show me the code</h1>
+    )
+}
